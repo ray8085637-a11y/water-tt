64280d4f39b3a10c7aa7ec24e8230dfd6b0c406a
ray8085637-a11y <ray8085637@gmail.com>
2025-08-20 19:38:52 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './index.html'
-    }
+    outDir: 'dist'
   }
 })
