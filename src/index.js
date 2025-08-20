61f4efbe85b9b719b7495a639c80f8a31756bbcb
ray8085637-a11y <ray8085637@gmail.com>
2025-08-20 20:14:40 +0900

    Delete src/index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';  // CSS 파일 import (중요!)
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
